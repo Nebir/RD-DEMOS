07ca39005ac225a7cce4fd135be1250e790ed24d
Nebir <nebir1993@gmail.com>
2018-04-11 17:11:12 +0600

    login page is done

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,43 +1,81 @@
 <template>
 	<div class="login-page">
 		<div class="container">
-			<div class="row">
-				<div class="col-md-6 ml-auto mr-auto">
-					<b-card>
-                        <img src="../assets/logo.png" class="img-fluid" alt="Logo" />
-						<b-form @submit="onSubmit" @reset="onReset" v-if="show">
+			<div class="row login-wrapper">
+				<div class="col-12 col-lg-5 ml-auto mr-auto align-self-center">
+					<b-card class="login-card">
+						<img src="../assets/logo.png" class="img-fluid mb-5" alt="Logo" />
+						<b-form @submit="onSubmit"
+										@reset="onReset"
+										v-if="show"
+										class="mb-4">
 							<b-form-group>
 								<b-form-input type="email"
-											  v-model="form.email"
-                                              size="lg"
-											  required
-											  placeholder="Email Address">
+															v-model="form.email"
+															size="lg"
+															required
+															placeholder="Email Address">
 								</b-form-input>
+								<span class="icon-box">
+									<i class="fa fa-envelope-o"></i>
+								</span>
 							</b-form-group>
 
 							<b-form-group>
 								<b-form-input type="password"
-											  v-model="form.password"
-                                              size="lg"
-											  required
-											  placeholder="Password">
+															v-model="form.password"
+															size="lg"
+															required
+															placeholder="Password">
 								</b-form-input>
+								<span class="icon-box">
+									<i class="fa fa-lock"></i>
+								</span>
 							</b-form-group>
 
-							<b-form-group id="exampleGroup4">
-                                <b-form-checkbox v-model="form.checked"
-                                                 class="self-align-start mr-auto">
-                                    Remember me
-                                </b-form-checkbox>
-                                <a to="/passwordReset"
-                                   class="self-align-end">
-                                    <span class="fa fa-lock"></span> Forgot Password?
-                                </a>
-							</b-form-group>
+							<b-row class="justify-content-between">
+								<b-col class="col-auto">
+									<b-form-checkbox v-model="form.checked"
+																	 class="self-align-start mr-auto"
+																	 style="font-size: 12px;">
+										Remember me
+									</b-form-checkbox>
+								</b-col>
 
-							<b-button type="submit" variant="primary">Submit</b-button>
-							<b-button type="reset" variant="danger">Reset</b-button>
+								<b-col class="col-auto">
+									<a to="/passwordReset"
+										 class="self-align-end"
+										 style="font-size: 12px;">
+										<span class="fa fa-lock"></span> Forgot Password?
+									</a>
+								</b-col>
+							</b-row>
+
+							<b-button size="lg"
+												type="submit"
+												class="btn-block btn-rd btn-teal mt-4 mb-3">
+								Log In
+							</b-button>
+
+							<a to="/">
+								<b-button size="lg"
+													type="submit"
+													class="btn-block btn-rd btn-skyblue">
+									Back to Main Menu
+								</b-button>
+							</a>
 						</b-form>
+
+						<b-form-text>
+							Didn't get the Verification Email?
+						</b-form-text>
+						<b-form-text>
+							<a to="/passwordReset"
+								 class="self-align-end"
+								 style="font-size: 12px;">
+								<span class="fa fa-envelope-o"></span> Resend verification Email
+							</a>
+						</b-form-text>
 					</b-card>
 				</div>
 			</div>
@@ -79,5 +117,49 @@
 </script>
 
 <style scoped>
-
+	.login-page .login-wrapper {
+		height: 100vh;
+	}
+	.form-group {
+		margin-bottom: 2rem;
+		position: relative;
+	}
+	span.icon-box {
+		position:  absolute;
+		right: 0;
+		width: 58px;
+		height: 58px;
+		border: 1px solid #213e7a;
+		color: #fff;
+		background-color: #213e7a;
+		border-radius: 50%;
+		display: table;
+		top: -5px;
+	}
+	.icon-box i {
+		display: table-cell;
+		vertical-align: middle;
+		font-size: 20px;
+	}
+	.form-control-lg {
+		-webkit-border-radius: 30px;
+		-moz-border-radius: 30px;
+		border-radius: 30px;
+		font-size: 1rem;
+		line-height: 2;
+	}
+	.form-control:focus {
+		border-color: #e6e7e8;
+		-webkit-box-shadow: 0 2px 1px 0px rgba(125, 127, 130, 0.25);
+		-moz-box-shadow: 0 2px 1px 0px rgba(125, 127, 130, 0.25);
+		box-shadow: 0 2px 1px 0px rgba(125, 127, 130, 0.25);
+	}
+	.custom-control-label {
+		margin-top: 4px;
+	}
+	.btn-rd {
+		-webkit-border-radius: 30px;
+		-moz-border-radius: 30px;
+		border-radius: 30px;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import {routes} from './router'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
